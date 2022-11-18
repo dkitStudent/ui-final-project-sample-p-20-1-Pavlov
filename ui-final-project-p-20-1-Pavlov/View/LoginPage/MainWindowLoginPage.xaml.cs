231c7e7fd21d8b2d3bc6072eb561465bb91c144c
dkitStudent <dkitneeds@gmail.com>
2022-11-18 10:03:07 +0300

    added new pages

--- ui-final-project-p-20-1-Pavlov/View/LoginPage/MainWindowLoginPage.xaml.cs
+++ ui-final-project-p-20-1-Pavlov/View/LoginPage/MainWindowLoginPage.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ui_final_project_p_20_1_Pavlov.Core;
+using ui_final_project_p_20_1_Pavlov.Model;
+using ui_final_project_p_20_1_Pavlov.View.EmployerPage;
 
 namespace ui_final_project_p_20_1_Pavlov.View.LoginPage
 {
@@ -23,6 +26,45 @@ namespace ui_final_project_p_20_1_Pavlov.View.LoginPage
         public MainWindowLoginPage()
         {
             InitializeComponent();
+        }
+
+        private void BtnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                User userModel = FrameNavigate.DB.Users.FirstOrDefault(u => u.UserMail == TbLogin.Text && u.UserPhone == PsbPassword.Password);
+                if (userModel == null)
+                {
+                    MessageBox.Show("Ошибка данных", "Системное сообщение", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                else
+                {
+                    switch (userModel.RoleID)
+                    {
+                        case 1:
+                            //FrameNavigate.FrameObject.Navigate(new MainAdministratorPage());
+                        case 2:
+                            //FrameNavigate.FrameObject.Navigate(new MainUserPage());
+                            break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message.ToString(), "Системная ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+
+            }
+        }
+
+        private void BtnWork_Click(object sender, RoutedEventArgs e)
+        {
+            FrameNavigate.FrameObject.Navigate(new MainEmployerPage());
+        }
+
+        private void BtnRegistration_Click(object sender, RoutedEventArgs e)
+        {
+            FrameNavigate.FrameObject.Navigate(new MainWindowRegistrationPage());
         }
     }
 }
